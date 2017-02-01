6bd385fb7448ff99291ce5940ca363e55d35db17
yuvalbl <yuval.bar.levi@gmail.com>
2017-02-01 14:14:39 +0200

    Prime ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,33 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { DataTableModule, SharedModule } from 'primeng/primeng';
+
 import { TMNTService } from './services/tmnt.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { Grid1Component } from './grid1/grid1.component';
 import { Grid2Component } from './grid2/grid2.component';
+import { NicknamePipe } from './pipes/nickname.pipe';
+import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     Grid1Component,
-    Grid2Component
+    Grid2Component,
+    NicknamePipe,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataTableModule,
+    SharedModule
   ],
   providers: [TMNTService],
   bootstrap: [AppComponent]
--- src/app/grid1/grid1.component.ts
+++ src/app/grid1/grid1.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { TMNTService } from '../services/tmnt.service';
 import { Character } from '../classes/character';
-
 
 
 @Component({
   selector: 'app-grid1',
   templateUrl: './grid1.component.html',
-  styleUrls: ['./grid1.component.css']
+  styleUrls: ['./grid1.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class Grid1Component implements OnInit {
+  public primeNGSummary;
+
+
+  public nameModes = [
+    { value: 0, display: 'standard'},
+    { value: 1, display: 'Nick Name'},
+    { value: 2, display: 'Better Nick Name'}
+  ];
+  public currentNameMode = 0;
+
   characters: Character[] = [];
   tmntService;
   constructor(tmntService: TMNTService) {
@@ -19,6 +29,19 @@ export class Grid1Component implements OnInit {
   ngOnInit(): void {
     this.tmntService.getCharacters()
       .then(data => this.characters = data);
+
+
+    this.primeNGSummary = this.getSummary();
   }
 
+  getSummary(): Object[] {
+    return [
+      { param: '2 Rows header',     value: 'yes' },
+      { param: 'Scroll body only',     value: 'yes' },
+      { param: 'Multi sort',     value: 'partial' },
+      { param: 'In header filters',     value: 'yes' },
+      { param: 'Filter by: category, text and range',     value: 'yes' },
+      { param: 'Custom html',     value: 'partial' },
+    ]
+  }
 }
